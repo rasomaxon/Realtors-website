6aba96772b9774fa18c806a2e1c284fe1e355e8b
rasomaxon <rasomaxon@mail.ru>
2025-05-03 16:34:26 +0300

    add adaptive

--- script.js
+++ script.js
@@ -2,9 +2,13 @@ window.addEventListener("scroll", function () {
   const header = document.querySelector("header");
   if (window.scrollY > 0) {
     header.classList.add("scrolled");
-    header.classList.remove("transparent");
   } else {
     header.classList.remove("scrolled");
-    header.classList.add("transparent");
   }
+});
+const burger = document.getElementById('burger');
+const navMenu = document.getElementById('navMenu');
+
+burger.addEventListener('click', () => {
+	navMenu.classList.toggle('active');
 });
